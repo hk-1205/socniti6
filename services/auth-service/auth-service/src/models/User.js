caa6baf829365f45a34b04c4814a6fcdf0bff06f
hk-1205 <hkhandelwal1205@gmail.com>
2026-03-30 20:15:39 +0530

    added auth-service

---
+++ services/auth-service/auth-service/src/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: false,
+      unique: true,
+      sparse: true,
+      lowercase: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: false,
+      unique: true,
+      sparse: true,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "organizer", "admin"],
+      default: "user",
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    otp: {
+      type: String,
+    },
+    otpExpires: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
